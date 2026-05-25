f89f8e8ef44e0f744e0d655371b9fc429a388634
Joaquin Hernandez Martinez <starseeker.code@gmail.com>
2026-05-25 21:05:35 +0200

    añadido tabulate

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from ui import msg_bienvenida, msg_salida_bucle_eventos, msg_input
 from tareas import añadir_tarea
+from tabulate import tabulate
 
 print(msg_bienvenida)
 
@@ -11,4 +12,6 @@ while True:
     if titulo_tarea.upper() in ("STOP", "PARA", "PARAR", "S"):
         break
     añadir_tarea(tareas, titulo_tarea.title())
-    print(tareas)  # TODO: Esto tiene que verse mas bonito
+    tabla_bonita = tabulate([(tarea["hecha"], tarea["titulo"]) for tarea in tareas],
+                            headers=["COMPLETADA", "TITULO"], tablefmt="rounded_grid")
+    print(f"\n{tabla_bonita}\n")
